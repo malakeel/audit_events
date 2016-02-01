494f9ee73cb00ecff013960389d66a61de3ad211
Mansour Al Akeel <mansour.alakeel@gmail.com>
2016-02-01 01:45:57 -0500

    Added flag to indicate modified tuples
    Can be filtered later for modified event only

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-# contrib/test_decoding/Makefile
+# contrib/audit_events/Makefile
 
 MODULE_big = audit
 OBJS = audit_events.o serializer.o
@@ -12,7 +12,7 @@ PG_CONFIG = pg_config
 PGXS := $(shell $(PG_CONFIG) --pgxs)
 include $(PGXS)
 else
-subdir = contrib/test_decoding
+subdir = contrib/audit_events
 top_builddir = ../..
 include $(top_builddir)/src/Makefile.global
 include $(top_srcdir)/contrib/contrib-global.mk
@@ -34,41 +34,41 @@ submake-regress:
 submake-isolation:
 	$(MAKE) -C $(top_builddir)/src/test/isolation all
 
-submake-test_decoding:
-	$(MAKE) -C $(top_builddir)/contrib/test_decoding
+submake-audit_events:
+	$(MAKE) -C $(top_builddir)/contrib/audit_events
 
 REGRESSCHECKS=ddl rewrite toast permissions decoding_in_xact decoding_into_rel binary prepared
 
-regresscheck: all | submake-regress submake-test_decoding
+regresscheck: all | submake-regress submake-audit_events
 	$(MKDIR_P) regression_output
 	$(pg_regress_check) \
-	    --temp-config $(top_srcdir)/contrib/test_decoding/logical.conf \
+	    --temp-config $(top_srcdir)/contrib/audit_events/logical.conf \
 	    --temp-install=./tmp_check \
-	    --extra-install=contrib/test_decoding \
+	    --extra-install=contrib/audit_events \
 	    --outputdir=./regression_output \
 	    $(REGRESSCHECKS)
 
-regresscheck-install-force: | submake-regress submake-test_decoding
+regresscheck-install-force: | submake-regress submake-audit_events
 	$(pg_regress_installcheck) \
-	    --extra-install=contrib/test_decoding \
+	    --extra-install=contrib/audit_events \
 	    $(REGRESSCHECKS)
 
 ISOLATIONCHECKS=mxact delayed_startup ondisk_startup concurrent_ddl_dml
 
-isolationcheck: all | submake-isolation submake-test_decoding
+isolationcheck: all | submake-isolation submake-audit_events
 	$(MKDIR_P) isolation_output
 	$(pg_isolation_regress_check) \
-	    --temp-config $(top_srcdir)/contrib/test_decoding/logical.conf \
-	    --extra-install=contrib/test_decoding \
+	    --temp-config $(top_srcdir)/contrib/audit_events/logical.conf \
+	    --extra-install=contrib/audit_events \
 	    --outputdir=./isolation_output \
 	    $(ISOLATIONCHECKS)
 
-isolationcheck-install-force: all | submake-isolation submake-test_decoding
+isolationcheck-install-force: all | submake-isolation submake-audit_events
 	$(pg_isolation_regress_installcheck) \
-	    --extra-install=contrib/test_decoding \
+	    --extra-install=contrib/audit_events \
 	    $(ISOLATIONCHECKS)
 
-PHONY: submake-test_decoding submake-regress check \
+PHONY: submake-audit_events submake-regress check \
 	regresscheck regresscheck-install-force \
 	isolationcheck isolationcheck-install-force
 
--- audit_events.c
+++ audit_events.c
@@ -6,8 +6,8 @@
 #include <catalog/pg_type.h>
  
 #include <nodes/parsenodes.h>
-#include "replication/output_plugin.h"
-#include "replication/logical.h"
+//#include "replication/output_plugin.h"
+#include <replication/logical.h>
  
 #include "utils/builtins.h"
 #include "utils/lsyscache.h"
@@ -16,7 +16,6 @@
 #include "utils/relcache.h"
 #include "utils/syscache.h"
 #include "utils/typcache.h"
- 
  
 #include <miscadmin.h>
 #include <commands/dbcommands.h>
@@ -32,71 +31,67 @@ extern void _PG_output_plugin_init(OutputPluginCallbacks *cb);
 
 
 static void audit_plugin_startup(LogicalDecodingContext *ctx,
-				 OutputPluginOptions *opt,
-				 bool is_init);
+								 OutputPluginOptions *opt,
+								 bool is_init);
 
 static void on_begin_txn(LogicalDecodingContext *ctx,
-			 ReorderBufferTXN *txn);
+						 ReorderBufferTXN *txn);
 
 static void audit_plugin_shutdown(LogicalDecodingContext *ctx);
 
 static void on_commit_txn(LogicalDecodingContext *ctx,
-			  ReorderBufferTXN *txn,
-			  XLogRecPtr commit_lsn);
+						  ReorderBufferTXN *txn,
+						  XLogRecPtr commit_lsn);
 
 static void on_row_change(LogicalDecodingContext *ctx,
-			  ReorderBufferTXN *txn,
-			  Relation rel,
-			  ReorderBufferChange *change);
+						  ReorderBufferTXN *txn,
+						  Relation rel,
+						  ReorderBufferChange *change);
 
 static void populate_fields(StringInfo s, TupleDesc tupdesc,
-			    HeapTuple oldtuple , HeapTuple newtuple ,
-			    EventData * event_data);
+							HeapTuple oldtuple , HeapTuple newtuple ,
+							EventData * event_data);
 
-
-static bool are_equal(HeapTupleHeader record1 , HeapTupleHeader record2) ;
-
+static bool are_equal( HeapTuple tuple1,
+					   HeapTuple tuple2,
+					   TupleDesc tupleDesc ) ;
 
 void _PG_init(void)
 {
-  /* other plugins can perform things here */
+	/* other plugins can perform things here */
 }
 
 /* specify output plugin callbacks */
 void _PG_output_plugin_init(OutputPluginCallbacks *cb)
 {
-  AssertVariableIsOfType(&_PG_output_plugin_init, LogicalOutputPluginInit);
+	AssertVariableIsOfType(&_PG_output_plugin_init, LogicalOutputPluginInit);
 
-  cb->startup_cb = audit_plugin_startup;
-  cb->begin_cb = on_begin_txn;
-  cb->change_cb = on_row_change;
-  cb->commit_cb = on_commit_txn;
-  cb->shutdown_cb = audit_plugin_shutdown;
-
-	 
-
-
+	cb->startup_cb = audit_plugin_startup;
+	cb->begin_cb = on_begin_txn;
+	cb->change_cb = on_row_change;
+	cb->commit_cb = on_commit_txn;
+	cb->shutdown_cb = audit_plugin_shutdown;
+  
 }
 
 /* initialize this plugin */
-static void audit_plugin_startup(LogicalDecodingContext *ctx, OutputPluginOptions *opt, bool is_init)
+static void audit_plugin_startup(LogicalDecodingContext *ctx,
+								 OutputPluginOptions *opt, bool is_init)
 {
 
-  EventData *data;
+	EventData *data;
   
-  data = palloc0(sizeof(EventData));
+	data = palloc0(sizeof(EventData));
 
-  data->context = AllocSetContextCreate(ctx->context,
-					"text conversion context",
-					ALLOCSET_DEFAULT_MINSIZE,
-					ALLOCSET_DEFAULT_INITSIZE,
-					ALLOCSET_DEFAULT_MAXSIZE);
+	data->context = AllocSetContextCreate(ctx->context,
+										  "text conversion context",
+										  ALLOCSET_DEFAULT_MINSIZE,
+										  ALLOCSET_DEFAULT_INITSIZE,
+										  ALLOCSET_DEFAULT_MAXSIZE);
   
-  ctx->output_plugin_private = data;
+	ctx->output_plugin_private = data;
 
-  opt->output_type = OUTPUT_PLUGIN_TEXTUAL_OUTPUT;
-
-
+	opt->output_type = OUTPUT_PLUGIN_TEXTUAL_OUTPUT;
 	
 }
 
@@ -105,30 +100,30 @@ static void audit_plugin_startup(LogicalDecodingContext *ctx, OutputPluginOption
 static void on_begin_txn(LogicalDecodingContext *ctx, ReorderBufferTXN *txn)
 {
 
-  bool last_write = true ;
+	bool last_write = true ;
 	
-  OutputPluginPrepareWrite(ctx, last_write);
+	OutputPluginPrepareWrite(ctx, last_write);
 
-  appendStringInfo(ctx->out, "BEGIN %u", txn->xid);
+	appendStringInfo(ctx->out, "BEGIN %u", txn->xid);
 
-  OutputPluginWrite(ctx, last_write);
+	OutputPluginWrite(ctx, last_write);
 }
 
 
 /* COMMIT callback */
 static void on_commit_txn(LogicalDecodingContext *ctx, ReorderBufferTXN *txn, XLogRecPtr commit_lsn)
 {
-  OutputPluginPrepareWrite(ctx, true);
-  appendStringInfo(ctx->out, "COMMIT Transaction %u", txn->xid);
-  OutputPluginWrite(ctx, true);
+	OutputPluginPrepareWrite(ctx, true);
+	appendStringInfo(ctx->out, "COMMIT Transaction %u", txn->xid);
+	OutputPluginWrite(ctx, true);
 }
 
 /* cleanup this plugin's resources */
 static void audit_plugin_shutdown(LogicalDecodingContext *ctx)
 {
-  EventData *data = ctx->output_plugin_private;
-  /* cleanup our own resources via memory context reset */
-  MemoryContextDelete(data->context);
+	EventData *data = ctx->output_plugin_private;
+	/* cleanup our own resources via memory context reset */
+	MemoryContextDelete(data->context);
 }
 
 
@@ -138,190 +133,222 @@ static void audit_plugin_shutdown(LogicalDecodingContext *ctx)
 static void on_row_change(LogicalDecodingContext *ctx, ReorderBufferTXN *txn, Relation relation, ReorderBufferChange *change)
 {
 
-  EventData *data;
-  Form_pg_class class_form;
-  TupleDesc	tupdesc;
-  MemoryContext old;
-  //char * schema ;
-  HeapTuple oldtuple;
-  HeapTuple newtuple;
+	EventData *data;
+	Form_pg_class class_form;
+	TupleDesc	tupdesc;
+	MemoryContext old;
+	//char * schema ;
+	HeapTuple oldtuple;
+	HeapTuple newtuple;
 
-  data = ctx->output_plugin_private;
+	data = ctx->output_plugin_private;
 
-  class_form = RelationGetForm(relation);
+	class_form = RelationGetForm(relation);
 
-  tupdesc = RelationGetDescr(relation);
+	tupdesc = RelationGetDescr(relation);
 
-  /* Avoid leaking memory by using and resetting our own context */
-  old = MemoryContextSwitchTo(data->context);
+	/* Avoid leaking memory by using and resetting our own context */
+	old = MemoryContextSwitchTo(data->context);
 	
-  data->tx_id = txn->xid ;
+	data->tx_id = txn->xid ;
 	
-  data->database = get_database_name(MyDatabaseId);
+	data->database = get_database_name(MyDatabaseId);
 
-  data->schema = get_namespace_name(get_rel_namespace(RelationGetRelid(relation)));
+	data->schema = get_namespace_name(get_rel_namespace(RelationGetRelid(relation)));
 		
-  data->table =  NameStr(class_form->relname) ;
+	data->table =  NameStr(class_form->relname) ;
 
-  //data->rel_id = class_form->rel ;
+	//data->rel_id = class_form->rel ;
 
-  sprintf(data->time_stamp , "%s",  timestamptz_to_str(txn->commit_time));
+	sprintf(data->time_stamp , "%s",  timestamptz_to_str(txn->commit_time));
 
-  oldtuple =  &change->data.tp.oldtuple->tuple ;
-  newtuple =  &change->data.tp.newtuple->tuple ;
+	oldtuple =  &change->data.tp.oldtuple->tuple ;
+	newtuple =  &change->data.tp.newtuple->tuple ;
 	
-  switch (change->action)
+	switch (change->action)
     {
     case REORDER_BUFFER_CHANGE_INSERT:
-      strcpy(data->action,"INSERT");
-      populate_fields(ctx->out, tupdesc, NULL , newtuple, data);
-      break;
+		strcpy(data->action,"INSERT");
+		populate_fields(ctx->out, tupdesc, NULL , newtuple, data);
+		break;
 		
     case REORDER_BUFFER_CHANGE_UPDATE:
-      strcpy(data->action,"UPDATE");
-      populate_fields(ctx->out, tupdesc, oldtuple , newtuple, data);
-      break;
+		strcpy(data->action,"UPDATE");
+		populate_fields(ctx->out, tupdesc, oldtuple , newtuple, data);
+		break;
 			
     case REORDER_BUFFER_CHANGE_DELETE:
-      strcpy(data->action,"DELETE");
-      populate_fields(ctx->out, tupdesc, oldtuple , NULL , data);
-      break;
+		strcpy(data->action,"DELETE");
+		populate_fields(ctx->out, tupdesc, oldtuple , NULL , data);
+		break;
 			
     default:
-      Assert(false);
+		Assert(false);
     }
-	
-  OutputPluginPrepareWrite(ctx, true);	
-  serialize( ctx->out , data ) ;
-	
-  free(data->fields);
-	
-  MemoryContextSwitchTo(old);
-  MemoryContextReset(data->context);
 
-  OutputPluginWrite(ctx, true);
+
+	OutputPluginPrepareWrite(ctx, true);	
+	serialize( ctx->out , data ) ;
+	
+	free(data->fields);
+	
+	MemoryContextSwitchTo(old);
+	MemoryContextReset(data->context);
+
+	OutputPluginWrite(ctx, true);
 }
 
 
 static void populate_fields(StringInfo s, TupleDesc tupdesc, HeapTuple oldtuple , HeapTuple newtuple , EventData * event_data)
 {
-  int	num_att;
-  int idx = 0; 
-  Field * field  ;
+	int	num_att;
+	int idx = 0; 
+	Field * field  ;
 
-  bool dirty = true ;
+	bool dirty = true ;
   
-  num_att = tupdesc->natts ;
+	num_att = tupdesc->natts ;
 
-  event_data->_fields_count = num_att ;
+	event_data->_fields_count = num_att ;
 	
-  event_data->fields = malloc (sizeof(Field) * tupdesc->natts);
+	event_data->fields = malloc (sizeof(Field) * tupdesc->natts);
 	
-  field = event_data->fields ;
-
-
+	field = event_data->fields ;
 
   
-  /* print all columns individually */
-  for (idx = 0; idx < num_att; idx++)
+	/* print all columns individually */
+	for (idx = 0; idx < num_att; idx++)
     {
-      Form_pg_attribute attr; /* the attribute itself */
-      //Oid			type_id;		/* type of current attribute */
-      Oid			typoutput;	/* output function */
-      bool		typisvarlena;
-      bool		is_old_null;		/* column is null? */
-      bool		is_new_null;		/* column is null? */
+		Form_pg_attribute attr; /* the attribute itself */
+		//Oid			type_id;		/* type of current attribute */
+		Oid			typoutput;	/* output function */
+		bool		typisvarlena;
+		bool		is_old_null;		/* column is null? */
+		bool		is_new_null;		/* column is null? */
 
-      Datum		old_val;	/* possibly toasted Datum */
-      Datum		new_val;	/* possibly toasted Datum */
-      attr = tupdesc->attrs[idx];
+		Datum		old_val;	/* possibly toasted Datum */
+		Datum		new_val;	/* possibly toasted Datum */
+		attr = tupdesc->attrs[idx];
 
-      /*
-       * Ignore dropped columns, we can't be sure everything is available for them
-       */
-      if (attr->attisdropped)
-	continue;
+		/*
+		 * Ignore dropped columns, we can't be sure everything is available for them
+		 */
+		if (attr->attisdropped)
+			continue;
       
-      /*
-       * Ignore system columns, oid will already have been printed if present.
-       */
-      if (attr->attnum < 0)
-	continue;
+		/*
+		 * Ignore system columns, oid will already have been printed if present.
+		 */
+		if (attr->attnum < 0)
+			continue;
 
-      field->ordinal = idx + 1 ;
+		field->ordinal = idx + 1 ;
 		
-      field->type_id = attr->atttypid ;
+		field->type_id = attr->atttypid ;
 		
-      sprintf(field->name ,"%s", quote_identifier(NameStr(attr->attname))) ;
+		sprintf(field->name ,"%s", quote_identifier(NameStr(attr->attname))) ;
 		
-      //sprintf(field->type ,"%s", format_type_be(type_id));
+		//sprintf(field->type ,"%s", format_type_be(type_id));
 		
-      /* query output function */
-      getTypeOutputInfo(field->type_id,  &typoutput, &typisvarlena);
+		/* query output function */
+		getTypeOutputInfo(field->type_id,  &typoutput, &typisvarlena);
 
-      field->output_type = typoutput ;
-      if(oldtuple != NULL )
-	{
-	  /* get Datum from tuple */
-	  old_val = heap_getattr(oldtuple, idx + 1, tupdesc, &is_old_null);
-	  //sprintf(field->old_val  ,"%s", OidOutputFunctionCall(typoutput, old_val));
-	  field->old_val = old_val;
-	}else
-	field->old_val = (Datum) NULL;
-      if (newtuple != NULL){
-	new_val = heap_getattr(newtuple, idx + 1, tupdesc, &is_new_null);
-	//sprintf(field->new_val  ,"%s", OidOutputFunctionCall(typoutput, new_val));
-	field->new_val = new_val;
-      }else
-	field->new_val = (Datum) NULL;
+		field->output_type = typoutput ;
+		if(oldtuple != NULL )
+		{
+			/* get Datum from tuple */
+			old_val = heap_getattr(oldtuple, idx + 1, tupdesc, &is_old_null);
+			//sprintf(field->old_val  ,"%s", OidOutputFunctionCall(typoutput, old_val));
+			field->old_val = old_val;
+		}else
+			field->old_val = (Datum) NULL;
+		if (newtuple != NULL){
+			new_val = heap_getattr(newtuple, idx + 1, tupdesc, &is_new_null);
+			//sprintf(field->new_val  ,"%s", OidOutputFunctionCall(typoutput, new_val));
+			field->new_val = new_val;
+		}else
+			field->new_val = (Datum) NULL;
 
-      if(oldtuple != NULL && newtuple != NULL)
-      	dirty = dirty && are_equal(oldtuple->t_data , newtuple->t_data) ;
+		//if(oldtuple != NULL && newtuple != NULL)
 
-      event_data->is_modified = dirty ;
 
-      field++;
+		event_data->is_modified = dirty ;
+
+		field++;
 
     }
+	event_data->is_modified = !are_equal(oldtuple , newtuple , tupdesc) ;
 }
 
 
-bool are_equal(	HeapTupleHeader record1 , HeapTupleHeader record2)
+bool are_equal(	HeapTuple tuple1 , HeapTuple tuple2 , TupleDesc tupleDesc ) 
 {
 
-  Datum res ;
-  bool result = false ;
-
-
-  /* if (record1 == NULL || record2 == NULL) */
-  /*   return false; */
+	Datum res ;
   
-  FmgrInfo *flinfo = palloc(sizeof(FmgrInfo));
-  FunctionCallinfodata fcinfo;  /* or allocate this somewhere */
+	bool result ;
+  
+	FmgrInfo *flinfo ;
+  
+	FunctionCallInfoData fcinfo;  /* or allocate this somewhere */
+
+	if (tuple1 == NULL || tuple2 == NULL)
+		return false;
+
+	flinfo = palloc(sizeof(FmgrInfo));
  
-  fmgr_info(fmgr_internal_function("record_image_eq"), flinfo);
+	fmgr_info(fmgr_internal_function("record_image_eq"), flinfo);
  
-  /* need this for each new fcinfo, but one fcinfo can be reused for multiple calls */
-  InitFunctionCallInfoData(fcinfo, flinfo,
-			   2, /* no. of args */
-			   InvalidOid,  /* collation */
-			   NULL, /* context - NULL if not called from sql */
-			   NULL /* resultinfo - NULL if not expected to return sets */);
+	/* need this for each new fcinfo, but one fcinfo can be reused for multiple calls */
+	InitFunctionCallInfoData(fcinfo, flinfo,
+							 2, /* no. of args */
+							 InvalidOid,  /* collation */
+							 NULL, /* context - NULL if not called from sql */
+							 NULL /* resultinfo - NULL if not expected to return sets */);
 
-  /* following is needed per call: */
-  fcinfo.arg[0] = record1;
-  fcinfo.arg[1] = record2;
-  fcinfo.argnull[0] = false;
-  fcinfo.argnull[1] = false;
+	/* following is needed per call: */
+	fcinfo.arg[0] =  heap_copy_tuple_as_datum(tuple1 , tupleDesc);
+	fcinfo.arg[1] =  heap_copy_tuple_as_datum(tuple2 , tupleDesc);
+	fcinfo.argnull[0] = false;
+	fcinfo.argnull[1] = false;
  
-  res  =  FunctionCallInvoke(&fcinfo);
+	res  =  FunctionCallInvoke(&fcinfo);
+	result  = DatumGetBool(res) ;
+  
+	pfree(flinfo) ;
 
-  result  = DatumGetBool(res) ;
-  pfree(flinfo) ;
-
-
-  //  record_image_eq();
-  return result ;
+	//  record_image_eq();
+	return result ;
     
 }
 
+ 
+/* bool _are_equal_(Datum d1 , Datum d2 ){ */
+  
+/*   Datum res ; */
+/*   bool result = false ; */
+/*   FmgrInfo *flinfo = palloc(sizeof(FmgrInfo)); */
+/*   FunctionCallInfoData fcinfo;  /\* or allocate this somewhere *\/ */
+		  
+/*   fmgr_info(fmgr_internal_function("record_image_eq"), flinfo); */
+		     
+/*   /\* need this for each new fcinfo, but one fcinfo can be reused for multiple calls *\/ */
+/*   InitFunctionCallInfoData(fcinfo, flinfo, */
+/* 			   2, /\* no. of args *\/ */
+/* 			   InvalidOid,  /\* collation *\/ */
+/* 			   NULL, /\* context - NULL if not called from sql *\/ */
+/* 			   NULL /\* resultinfo - NULL if not expected to return sets *\/); */
+																      
+/*   /\* following is needed per call: *\/ */
+/*   fcinfo.arg[0] = d1; */
+/*   fcinfo.arg[1] = d2; */
+/*   fcinfo.argnull[0] = false; */
+/*   fcinfo.argnull[1] = false; */
+  
+/*   res  = (Datum) FunctionCallInvoke(&fcinfo); */
+  
+/*   result  = DatumGetBool(res) ; */
+  
+/*   pfree(flinfo) ; */
+/*   return result ; */
+/* } */
